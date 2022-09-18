ee68531a296fc861bde57c2099715852da6b11d7
astra <punkx.0gravity@gmail.com>
2022-09-18 12:24:53 +0545

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 printf "Enter name of File:"
 read -r name
-nmap $1 -n -sP | grep report | awk '{print $5}'
-nmap $1 -n -sP | grep report | awk '{print $5}' >> $name
+nmap $1 -n -sP -Pn | grep report | awk '{print $5}'
+nmap $1 -n -sP -Pn | grep report | awk '{print $5}' >> $name
