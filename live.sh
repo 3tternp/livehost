305c2489cdab7fd9e9c4532eb6b8de28824ab3b0
astra <26215963+3tternp@users.noreply.github.com>
2023-09-27 23:53:40 +0545

    Update live.sh

--- live.sh
+++ live.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+# Prompt the user for the name of a file
 printf "Enter the name of result File:"
 read -r name
 
+# Scan the network for active hosts
 nmap $1 -n -sP | grep report | awk '{print $5}'
+
+# Print a livehost to the user
 nmap $1 -n -sP | grep report | awk '{print $5}' >> $name
